26f52fb3f338526e1cb27e579a6c89351612b777
suhyun9764 <suhyun9764@naver.com>
2024-10-10 02:44:55 +0900

    refactor(): test 환경 변경

--- src/test/java/com/example/couplesharebackend/CoupleShareBackendApplicationTests.java
+++ src/test/java/com/example/couplesharebackend/CoupleShareBackendApplicationTests.java
@@ -3,7 +3,11 @@ package com.example.couplesharebackend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+    "spring.datasource.url=jdbc:mysql://localhost:3306/coupleshare?useSSL=false&useUnicode=true&serverTimezone=Asia/Seoul&allowPublicKeyRetrieval=true",
+    "spring.datasource.username=root",
+    "spring.datasource.password=coupleshare9764"
+})
 class CoupleShareBackendApplicationTests {
 
     @Test
